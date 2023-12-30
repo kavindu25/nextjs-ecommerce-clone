85b29a2771505fd15a4afd629a0a3ac78b3f5b04
Kavindu <kavindudealwis@gmail.com>
2023-12-30 21:08:03 +0530

    fix: styling fixes

--- src/app/_heros/CustomHero/index.tsx
+++ src/app/_heros/CustomHero/index.tsx
@@ -17,18 +17,20 @@ export const CustomHero: React.FC<Page['hero']> = ({ richText, media, links }) =
   return (
     <section className={classes.hero}>
       <div className={classes.heroWrapper} style={{ backgroundImage: `url(${mediaUrl})` }}>
-        <RichText content={richText} />
-        {Array.isArray(links) && links.length > 0 && (
-          <ul className={classes.links}>
-            {links.map(({ link }, i) => {
-              return (
-                <li key={i}>
-                  <CMSLink {...link} />
-                </li>
-              )
-            })}
-          </ul>
-        )}
+        <div className={classes.heroTextBox}>
+          <RichText content={richText} />
+          {Array.isArray(links) && links.length > 0 && (
+            <ul className={classes.links}>
+              {links.map(({ link }, i) => {
+                return (
+                  <li key={i}>
+                    <CMSLink {...link} />
+                  </li>
+                )
+              })}
+            </ul>
+          )}
+        </div>
       </div>
     </section>
   )
