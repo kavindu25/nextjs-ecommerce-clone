02a78184ef8ff16e78635857a4397dc18ef92d2f
Kavindu <kavindudealwis@gmail.com>
2024-01-02 20:38:29 +0530

    feat: added custom styles

--- src/app/_components/Promotion/index.tsx
+++ src/app/_components/Promotion/index.tsx
@@ -3,9 +3,7 @@ import React from 'react'
 import classes from './index.module.scss'
 
 const Promotion = () => {
-  return (
-    <div>Promotion</div>
-  )
+  return <div>Promotion</div>
 }
 
 export default Promotion
