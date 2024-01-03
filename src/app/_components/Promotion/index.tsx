0bc9ffd9edb5ed4368ed04eed3395d62c7468ec9
Kavindu <kavindudealwis@gmail.com>
2024-01-03 16:39:54 +0530

    feat: added promotion layout

--- src/app/_components/Promotion/index.tsx
+++ src/app/_components/Promotion/index.tsx
@@ -3,7 +3,24 @@ import React from 'react'
 import classes from './index.module.scss'
 
 const Promotion = () => {
-  return <div>Promotion</div>
+  return (
+    <section className={classes.promotion}>
+      <div className={classes.textBox}>
+        <h3 className={classes.title}>Deals of the month</h3>
+        <p>
+          Get redy for a shopping experience like never before with our Deals of the Month! Every
+          purchase comes with exclusive perks and offers, making this month a celebration of savvy
+          choices and amazing deals. Don't miss out!
+        </p>
+        <ul className={classes.stats}>
+          <li className={classes.statBox}>
+            <h4>5</h4>
+            <p>Days</p>
+          </li>
+        </ul>
+      </div>
+    </section>
+  )
 }
 
 export default Promotion
