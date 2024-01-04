4aed62b695395fdd2f9fd0dde9920dbcf80abb9c
Kavindu <kavindudealwis@gmail.com>
2024-01-04 21:11:10 +0530

    feat:added promo countdown timer

--- src/app/_components/Promotion/index.tsx
+++ src/app/_components/Promotion/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import classes from './index.module.scss'
 
@@ -11,6 +11,31 @@ const Promotion = () => {
     minutes: 0,
     seconds: 0,
   })
+
+  const targetDate = new Date()
+  targetDate.setDate(targetDate.getDate() + 7)
+
+  useEffect(() => {
+    const timerInterval = setInterval(() => {
+      const currentTime = new Date()
+      const timeDifference = Math.max(Number(targetDate) - Number(currentTime), 0)
+
+      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
+      const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+      const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60))
+      const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000)
+
+      setTime({ days, hours, minutes, seconds })
+
+      if (timeDifference === 0) {
+        clearInterval(timerInterval)
+      }
+    }, 1000)
+
+    return () => {
+      clearInterval(timerInterval)
+    }
+  }, [])
 
   return (
     <section className={classes.promotion}>
