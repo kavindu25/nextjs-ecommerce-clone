b4a7088abd522834aa0345e288cab0e11b5bf5f7
Kavindu <kavindudealwis@gmail.com>
2024-01-08 21:14:02 +0530

    feat: added filters component layout

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Checkbox } from 'payload/components/forms'
 
-const Filters = () => {
+import { Category } from '../../../../payload/payload-types'
+import { useFilter } from '../../../_providers/Filter'
+
+import classes from './index.module.scss'
+
+const Filters = ({ categories }: { categories: Category[] }) => {
+  const { categoryFilters, sort, setCategoryFilters, setSort } = useFilter()
+
+  const handleCategories = (categoryId: string) => {}
   return (
-    <div>Filters</div>
+    <div className={classes.filters}>
+      <div>
+        <h6 className={classes.title}>Product Categories</h6>
+        <div className={classes.categories}>
+          {categories.map(category => {
+            const isSelected = false
+
+            // return <Checkbox key={category.id} label={category.title} />
+          })}
+        </div>
+      </div>
+    </div>
   )
 }
 
