dd9df10982ee55f88ced25c35f96774d96e50dcd
Kavindu <kavindudealwis@gmail.com>
2024-01-08 23:50:39 +0530

    feat:added custom radio in filtere component

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 import { Category } from '../../../../payload/payload-types'
 import { Checkbox } from '../../../_components/Checkbox'
 import { HR } from '../../../_components/HR'
+import { RadioButton } from '../../../_components/Radio'
 import { useFilter } from '../../../_providers/Filter'
 
 import classes from './index.module.scss'
@@ -34,7 +35,15 @@ const Filters = ({ categories }: { categories: Category[] }) => {
         </div>
         <HR className={classes.hr} />
         <h6 className={classes.title}>Sort By</h6>
-        <div className={classes.categories}></div>
+        <div className={classes.categories}>
+          <RadioButton
+            label="Latest"
+            value="-createdAt"
+            isSelected={sort === '-createdAt'}
+            onRadioChange={handleSort}
+            groupName="sort"
+          />
+        </div>
       </div>
     </div>
   )
