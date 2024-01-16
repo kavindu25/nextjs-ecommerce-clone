ebfffe71b92b4e41f36188ced9bb5b67a842af32
Kavindu <kavindudealwis@gmail.com>
2024-01-17 00:24:16 +0530

    feat: added cartitem component

--- src/app/(pages)/cart/CartItem/index.tsx
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -1,11 +1,61 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Media } from '../../../_components/Media'
+import { Price } from '../../../_components/Price'
 
 import classes from './index.module.scss'
 
-const CartItem = () => {
+const CartItem = ({ product, title, metaImage, qty, addItemToCart }) => {
+  const [quantity, setQuantity] = useState(qty)
+
+  const decrementQty = () => {}
+  const incrementQty = () => {}
+  const enterQty = () => {}
+
   return (
-    <li className={classes.item}>
-      <h6>title</h6>
+    <li className={classes.item} key={title}>
+      <Link href={`/products/${product.slug}`} className={classes.mediaWrapper}>
+        {!metaImage && <span>No image</span>}
+        {metaImage && typeof metaImage !== 'string' && (
+          <Media className={classes.media} imgClassName={classes.image} resource={metaImage} fill />
+        )}
+      </Link>
+      <div className={classes.itemDetails}>
+        <div className={classes.titleWrapper}>
+          <h6>{title}</h6>
+          <Price product={product} button={false} />
+        </div>
+        <div className={classes.quantity}>
+          <div className={classes.quantityButton} onClick={decrementQty}>
+            <Image
+              src="/assets/icons/minus.svg"
+              alt="minus"
+              width={24}
+              height={24}
+              className={classes.qtnBt}
+            />
+          </div>
+          <input
+            type="text"
+            className={classes.quantityInput}
+            value={quantity}
+            onChange={enterQty}
+          />
+          <div className={classes.quantityButton} onClick={incrementQty}>
+            <Image
+              src="/assets/icons/plus.svg"
+              alt="plus"
+              width={24}
+              height={24}
+              className={classes.qtnBt}
+            />
+          </div>
+        </div>
+      </div>
     </li>
   )
 }
